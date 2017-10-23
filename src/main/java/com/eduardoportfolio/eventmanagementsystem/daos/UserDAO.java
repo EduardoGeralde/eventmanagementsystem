7db99eb2e277f185a471e1fa52706922fe506de9
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 13:03:55 -0200

    UserDAO created, CRUD with spring data

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/UserDAO.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/UserDAO.java
@@ -1,7 +1,11 @@
 package com.eduardoportfolio.eventmanagementsystem.daos;
 
+import com.eduardoportfolio.eventmanagementsystem.models.User;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Eduardo on 23/10/17.
  */
-public interface UserDAO {
+public interface UserDAO extends CrudRepository <User, Long>{
+
 }
