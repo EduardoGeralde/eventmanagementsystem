8e6a7de21fcfd1ed5a2a923eaccda84b50270cdd
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-22 22:18:57 -0200

    first commit

---
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/EventmanagementsystemApplication.java
@@ -0,0 +1,12 @@
+package com.eduardoportfolio.eventmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanagementsystemApplication.class, args);
+	}
+}
