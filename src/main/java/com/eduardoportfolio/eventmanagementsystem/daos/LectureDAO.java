aca6f74acb100471eb28d70eefb47e7507160cb1
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 13:03:31 -0200

    LectureDAO created, CRUD with spring data

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/LectureDAO.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/LectureDAO.java
@@ -1,7 +1,11 @@
 package com.eduardoportfolio.eventmanagementsystem.daos;
 
+import com.eduardoportfolio.eventmanagementsystem.models.Lecture;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Eduardo on 23/10/17.
  */
-public interface LectureDAO {
+public interface LectureDAO extends CrudRepository <Lecture, Long> {
+
 }
