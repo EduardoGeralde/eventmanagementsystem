9b25d6360e4d8bd1c777ae5a5011d18f9cec091b
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 13:05:40 -0200

    improved

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/models/User.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/models/User.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String userName;
     private String userEmail;
