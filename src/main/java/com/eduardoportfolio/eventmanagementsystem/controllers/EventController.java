97753c3ba03200842dbead864747f785ac2187af
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 15:44:29 -0200

    EventController created, list action added

---
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventController.java
@@ -0,0 +1,30 @@
+package com.eduardoportfolio.eventmanagementsystem.controllers;
+
+import com.eduardoportfolio.eventmanagementsystem.daos.EventDAO;
+import com.eduardoportfolio.eventmanagementsystem.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Eduardo on 23/10/17.
+ */
+@Controller
+public class EventController {
+
+    EventDAO eventDAO;
+
+    @Autowired
+    public EventController(EventDAO eventDAO) {
+        this.eventDAO = eventDAO;
+    }
+
+    @RequestMapping(value = "/listEvents", method = RequestMethod.GET)
+    public String listEvents(Model model){
+        model.addAttribute("events", eventDAO.findAll());
+        return "events";
+    }
+
+}
