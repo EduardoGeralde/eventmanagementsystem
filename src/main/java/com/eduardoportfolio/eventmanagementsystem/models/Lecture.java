1f7488a64196ba4c8b83c512ff8624fc64e9dcd7
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 13:05:28 -0200

    improved

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Lecture.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Lecture.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Lecture {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long lectureId;
     @ManyToOne
     private User lectureUser;
