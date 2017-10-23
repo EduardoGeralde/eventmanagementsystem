8363a1a8a0a14b3c0653738a8aa551e9a3a2900c
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 08:11:57 -0200

    Lecture Pojo class created

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Lecture.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Lecture.java
@@ -1,7 +1,54 @@
 package com.eduardoportfolio.eventmanagementsystem.models;
 
+
 /**
  * Created by Eduardo on 23/10/17.
  */
 public class Lecture {
+
+    private Long lectureId;
+    private User userLecturer;
+    private String lectureTitle;
+    private String lectureDescription;
+    private Event event;
+
+    public Long getLectureId() {
+        return lectureId;
+    }
+
+    public void setLectureId(Long lectureId) {
+        this.lectureId = lectureId;
+    }
+
+    public User getUserLecturer() {
+        return userLecturer;
+    }
+
+    public void setUserLecturer(User userLecturer) {
+        this.userLecturer = userLecturer;
+    }
+
+    public String getLectureTitle() {
+        return lectureTitle;
+    }
+
+    public void setLectureTitle(String lectureTitle) {
+        this.lectureTitle = lectureTitle;
+    }
+
+    public String getLectureDescription() {
+        return lectureDescription;
+    }
+
+    public void setLectureDescription(String lectureDescription) {
+        this.lectureDescription = lectureDescription;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
