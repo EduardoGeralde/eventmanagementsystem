2c57c1065c5fef07dfdb7efbc6271df46a131add
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-23 13:13:48 -0200

    hash code, equals and toString methods added

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/models/User.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/models/User.java
@@ -69,4 +69,28 @@ public class User {
     public void setUserLectures(Collection<Lecture> userLectures) {
         this.userLectures = userLectures;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return userId != null ? userId.equals(user.userId) : user.userId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return userId != null ? userId.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                '}';
+    }
 }
