271df9336b3cf87516ae14d8ed456f26daa41814
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-01 21:36:50 -0200

    login controller created

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/LoginController.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/LoginController.java
@@ -1,7 +1,36 @@
 package com.eduardoportfolio.eventmanagementsystem.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
 /**
  * Created by Eduardo on 01/11/17.
  */
+@Controller
 public class LoginController {
+
+    RequestMapping("/login")
+    public String showLoginForm(Model model){
+        model.addAttribute("loginCommand", new LoginCommand());
+
+        return "loginForm";
+    }
+
+    @RequestMapping("/logout-success")
+    public String yourLoggedOut(){
+        return "logout-success";
+    }
+
+    public String doLogin(@Valid LoginComand loginComand, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "loginForm";
+        }
+
+        return "redirect:index";
+    }
 }
