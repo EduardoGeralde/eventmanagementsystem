e142cae8580ce2001eabed18e11ba7305601e3a2
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 15:38:26 -0200

    data script added to spring pre fill the bd

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('123', 'test1@gmail.com', '123');
+INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('456', 'test2@gmail.com', '456');
