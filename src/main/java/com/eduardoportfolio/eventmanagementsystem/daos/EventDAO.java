d6eaa44ab1ef50327264851db6c2e0b4e2487354
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 13:49:31 -0200

    same

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/EventDAO.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/EventDAO.java
@@ -1,7 +1,10 @@
 package com.eduardoportfolio.eventmanagementsystem.daos;
 
 import com.eduardoportfolio.eventmanagementsystem.models.Event;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by Eduardo on 23/10/17.
