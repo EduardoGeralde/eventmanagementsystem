17ecba20cf291567ab397532494531f373b1cc94
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 15:38:52 -0200

    unused imports removed

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/EventDAO.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/EventDAO.java
@@ -1,10 +1,8 @@
 package com.eduardoportfolio.eventmanagementsystem.daos;
 
 import com.eduardoportfolio.eventmanagementsystem.models.Event;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 /**
  * Created by Eduardo on 23/10/17.
