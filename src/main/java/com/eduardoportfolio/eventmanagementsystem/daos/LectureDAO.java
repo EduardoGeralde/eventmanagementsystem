421fcca9561ff241cd8149dce7ed74cf1e7b3417
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 13:49:46 -0200

    same

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/LectureDAO.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/daos/LectureDAO.java
@@ -3,6 +3,7 @@ package com.eduardoportfolio.eventmanagementsystem.daos;
 import com.eduardoportfolio.eventmanagementsystem.models.Lecture;
 import org.springframework.data.repository.CrudRepository;
 
+
 /**
  * Created by Eduardo on 23/10/17.
  */
