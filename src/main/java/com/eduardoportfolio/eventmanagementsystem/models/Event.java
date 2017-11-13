72f3e39fbebd655626e933e84a4c8035ff4ef0ce
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-13 16:13:38 -0200

    Data annotation inserted from project Lombok

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Event.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/models/Event.java
@@ -1,5 +1,6 @@
 package com.eduardoportfolio.eventmanagementsystem.models;
 
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -9,6 +10,8 @@ import java.util.*;
 /**
  * Created by Eduardo on 23/10/17.
  */
+
+@Data
 @Entity
 public class Event {
 
@@ -26,135 +29,9 @@ public class Event {
     private String eventLogoPath;
     @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
     private Calendar eventDate;
-    private boolean eventClosed;
+    private Boolean eventClosed = false;
     @ManyToOne
     private User eventUser;
     @OneToMany (cascade = CascadeType.ALL, mappedBy = "lectureEvent")
     private List<Lecture> eventLectures = new ArrayList<>();
-
-    public Long getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Long eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getEventDescription() {
-        return eventDescription;
-    }
-
-    public void setEventDescription(String eventDescription) {
-        this.eventDescription = eventDescription;
-    }
-
-    public String getEventSite() {
-        return eventSite;
-    }
-
-    public void setEventSite(String eventSite) {
-        this.eventSite = eventSite;
-    }
-
-    public String getEventLocal() {
-        return eventLocal;
-    }
-
-    public void setEventLocal(String eventLocal) {
-        this.eventLocal = eventLocal;
-    }
-
-    public String getEventLogoPath() {
-        return eventLogoPath;
-    }
-
-    public void setEventLogoPath(String eventLogoPath) {
-        this.eventLogoPath = eventLogoPath;
-    }
-
-    public Calendar getEventDate() {
-        return eventDate;
-    }
-
-    public void setEventDate(Calendar eventDate) {
-        this.eventDate = eventDate;
-    }
-
-    public Boolean getEventActive() {
-        return eventClosed;
-    }
-
-    public void setEventActive(Boolean eventActive) {
-        this.eventClosed = eventActive;
-    }
-
-    public User getEventUser() {
-        return eventUser;
-    }
-
-    public void setEventUser(User eventUser) {
-        this.eventUser = eventUser;
-    }
-
-    public boolean isEventActive() {
-        return eventClosed;
-    }
-
-    public void setEventActive(boolean eventActive) {
-        this.eventClosed = eventActive;
-    }
-
-    public boolean isEventClosed() {
-        return eventClosed;
-    }
-
-    public void setEventClosed(boolean eventClosed) {
-        this.eventClosed = eventClosed;
-    }
-
-    public List<Lecture> getEventLectures() {
-        return eventLectures;
-    }
-
-    public void setEventLectures(List<Lecture> eventLectures) {
-        this.eventLectures = eventLectures;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Event event = (Event) o;
-
-        return eventId != null ? eventId.equals(event.eventId) : event.eventId == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return eventId != null ? eventId.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "eventId=" + eventId +
-                ", eventName='" + eventName + '\'' +
-                ", eventDescription='" + eventDescription + '\'' +
-                ", eventSite='" + eventSite + '\'' +
-                ", eventLocal='" + eventLocal + '\'' +
-                ", eventLogoPath='" + eventLogoPath + '\'' +
-                ", eventDate=" + eventDate +
-                ", eventActive=" + eventClosed +
-                ", eventUser=" + eventUser +
-                '}';
-    }
 }
