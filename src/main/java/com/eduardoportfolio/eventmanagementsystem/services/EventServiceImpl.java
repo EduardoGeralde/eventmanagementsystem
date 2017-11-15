be335a66f2343970c81f3daaf542aa2d63973c6e
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 15:30:17 -0200

    maven failsafe plugin added

---
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/services/EventServiceImpl.java
@@ -0,0 +1,7 @@
+package com.eduardoportfolio.eventmanagementsystem.services;
+
+/**
+ * Created by Eduardo on 15/11/17.
+ */
+public class EventServiceImpl {
+}
---
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventControllerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 15/11/17.
+ */
+public class EventControllerTest {
+
+}
