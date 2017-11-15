b7f4681973f916701561a9e9a7446be94428c226
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 15:34:23 -0200

    Event Service Implementation

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/services/EventServiceImpl.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/services/EventServiceImpl.java
@@ -1,7 +1,31 @@
 package com.eduardoportfolio.eventmanagementsystem.services;
 
+import com.eduardoportfolio.eventmanagementsystem.daos.EventDAO;
+import com.eduardoportfolio.eventmanagementsystem.models.Event;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Eduardo on 15/11/17.
  */
-public class EventServiceImpl {
+@Slf4j
+@Service
+public class EventServiceImpl implements EventService{
+
+    private final EventDAO eventDAO;
+
+    public EventServiceImpl(EventDAO eventDAO) {
+        this.eventDAO = eventDAO;
+    }
+
+    @Override
+    public List<Event> getEvents() {
+        log.debug("EventServiceImpl getEvents");
+        List<Event> eventList = new ArrayList<>();
+        eventDAO.findAll().iterator().forEachRemaining(eventList::add);
+        return eventList;
+    }
 }
