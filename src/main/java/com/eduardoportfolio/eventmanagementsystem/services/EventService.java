3ffb87230a51b95b42b1179188152e26a9deddd2
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-15 15:33:59 -0200

    Event Service Interface created

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/services/EventService.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/services/EventService.java
@@ -1,7 +1,13 @@
 package com.eduardoportfolio.eventmanagementsystem.services;
 
+import com.eduardoportfolio.eventmanagementsystem.models.Event;
+
+import java.util.List;
+
 /**
  * Created by Eduardo on 15/11/17.
  */
 public interface EventService {
+
+    List<Event> getEvents();
 }
