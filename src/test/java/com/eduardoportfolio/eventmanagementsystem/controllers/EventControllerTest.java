acc282c880da0b04d222d7301a41bff1eb4f70a0
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-16 17:26:44 -0200

    improved showEvent

---
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventControllerTest.java
@@ -0,0 +1,32 @@
+package com.eduardoportfolio.eventmanagementsystem.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 16/11/17.
+ */
+public class EventControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void eventForm() throws Exception {
+
+    }
+
+    @Test
+    public void saveEvent() throws Exception {
+
+    }
+
+    @Test
+    public void getEventById() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/IndexControllerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eduardo on 16/11/17.
+ */
+public class IndexControllerTest {
+
+}
