2e15a152ee9017c426fa5b0b65527c83770413a8
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-18 17:23:57 -0200

    lecture command created and implemented

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/LectureCommand.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/LectureCommand.java
@@ -1,7 +1,19 @@
 package com.eduardoportfolio.eventmanagementsystem.commands;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by Eduardo on 18/11/17.
  */
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class LectureCommand {
+
+    private Long lectureId;
+    private String lectureTitle;
+    private String lectureDescription;
 }
