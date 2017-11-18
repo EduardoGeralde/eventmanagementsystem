577feb1b630e5e0805698de98e15a5615afe25d6
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-18 17:24:20 -0200

    user command created and implemented

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/UserCommand.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/UserCommand.java
@@ -1,7 +1,24 @@
 package com.eduardoportfolio.eventmanagementsystem.commands;
 
+import com.eduardoportfolio.eventmanagementsystem.models.Event;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Eduardo on 18/11/17.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserCommand {
+
+    private Long userId;
+    private String userName;
+    private String userEmail;
+    private String password;
+    private List<Event> events = new ArrayList<>();
 }
