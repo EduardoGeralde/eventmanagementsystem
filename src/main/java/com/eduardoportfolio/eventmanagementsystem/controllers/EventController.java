f6da030b69bcfcb97a84cb555ea34ce49a3b2913
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-22 15:59:31 -0200

    updateEvent and saveOrUpdate actions created, and eventForm action changed with EventCommand within Model parameter

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventController.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventController.java
@@ -1,5 +1,6 @@
 package com.eduardoportfolio.eventmanagementsystem.controllers;
 
+import com.eduardoportfolio.eventmanagementsystem.commands.EventCommand;
 import com.eduardoportfolio.eventmanagementsystem.daos.EventDAO;
 import com.eduardoportfolio.eventmanagementsystem.models.Event;
 import com.eduardoportfolio.eventmanagementsystem.services.EventService;
@@ -8,9 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Eduardo on 23/10/17.
@@ -27,15 +26,33 @@ public class EventController {
     }
 
     @RequestMapping(value = "/event/form", method = RequestMethod.GET)
-    public String eventForm(){
+    public String eventForm(Model model){
         log.debug("EventController eventForm");
+        model.addAttribute("event", new EventCommand());
         return "event/eventForm";
     }
 
-    @RequestMapping(value = "/event/{id}")
+    @RequestMapping(value = "/event/{id}/show")
     public String getEventById(@PathVariable("id")Long id, Model model){
         log.debug("EventController getEventById");
         model.addAttribute("event", eventService.getEventById(id));
         return "event/showEvent";
     }
+
+    @RequestMapping(value= "/event/{id}/update")
+    public String updateEvent(@PathVariable("id")Long id, Model model){
+        log.debug("EventController updateEvent");
+        model.addAttribute("event", eventService.findCommandById(id));
+        return "event/eventForm";
+    }
+
+    @PostMapping
+    @RequestMapping(value = "event")
+    public String saveOrUpdate(@ModelAttribute EventCommand eventCommand){
+        log.debug("EventController saveOrUpdate");
+        EventCommand savedEvent = eventService.saveEventCommand(eventCommand);
+        return "redirect:/event/"+savedEvent.getEventId()+"/show";
+    }
+
+
 }
