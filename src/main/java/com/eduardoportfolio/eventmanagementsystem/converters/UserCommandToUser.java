0a26dcdd0253a5abf7356cd6fb850d64da42ba8e
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-24 10:06:36 -0200

    EventCommandToEvent, LectureCommandToLecture deleted

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/converters/UserCommandToUser.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/converters/UserCommandToUser.java
@@ -1,7 +1,6 @@
 package com.eduardoportfolio.eventmanagementsystem.converters;
 
 import com.eduardoportfolio.eventmanagementsystem.commands.UserCommand;
-import com.eduardoportfolio.eventmanagementsystem.models.Lecture;
 import com.eduardoportfolio.eventmanagementsystem.models.User;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
@@ -14,14 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCommandToUser implements Converter<UserCommand, User> {
 
-    private final EventCommandToEvent eventConverter;
-    private final LectureCommandToLecture lectureConverter;
-
-    public UserCommandToUser(EventCommandToEvent eventConverter, LectureCommandToLecture lectureConverter) {
-        this.eventConverter = eventConverter;
-        this.lectureConverter = lectureConverter;
-    }
-
     @Synchronized
     @Nullable
     @Override
@@ -32,19 +23,9 @@ public class UserCommandToUser implements Converter<UserCommand, User> {
 
         User user = new User();
         user.setUserId(userCommand.getUserId());
-        user.setUsername(userCommand.getUserName());
+        user.setUsername(userCommand.getUsername());
         user.setPassword(userCommand.getPassword());
         user.setUserEmail(userCommand.getUserEmail());
-
-        if(userCommand.getEvents() != null && userCommand.getEvents().size() > 0){
-            userCommand.getEvents()
-                    .forEach(event -> user.getUserEvents().add(eventConverter.convert(event)));
-        }
-
-        if(userCommand.getLectures() != null && userCommand.getLectures().size() > 0){
-            userCommand.getLectures()
-                    .forEach(lecture -> user.getUserLectures().add(lectureConverter.convert(lecture)));
-        }
 
         return user;
     }
