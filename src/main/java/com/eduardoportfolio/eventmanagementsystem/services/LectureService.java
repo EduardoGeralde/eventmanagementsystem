c0a914c66991934e85b3072c97a4ce794b13e04b
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-25 15:06:42 -0200

    LectureService created

---
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/services/LectureService.java
@@ -0,0 +1,10 @@
+package com.eduardoportfolio.eventmanagementsystem.services;
+
+import com.eduardoportfolio.eventmanagementsystem.commands.LectureCommand;
+
+/**
+ * Created by Eduardo on 24/11/17.
+ */
+public interface LectureService {
+    LectureCommand findByEventIdAndLectureId(Long eventId, Long lectureId);
+}
