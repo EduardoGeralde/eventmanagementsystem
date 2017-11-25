164fc3643dc6189269ba3339cbcf1b38ec1a7b0a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-25 15:07:18 -0200

    usernames changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('123', 'test1@gmail.com', '123');
-INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('456', 'test2@gmail.com', '456');
+INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('123', 'test1@gmail.com', 'John P.');
+INSERT INTO USER_DETAIL (password, user_email, username) VALUES ('456', 'test2@gmail.com', 'Eduardo Geralde');
