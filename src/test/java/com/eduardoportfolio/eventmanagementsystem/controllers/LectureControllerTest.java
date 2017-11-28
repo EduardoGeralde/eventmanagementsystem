60d21f6f6ee92e1c497b0bc9d1b008ed2d88e1b3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-28 06:36:07 -0200

    UpdateIngredientForm and SaveOrUpdate test methods created

--- src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/LectureControllerTest.java
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/LectureControllerTest.java
@@ -8,15 +8,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -71,5 +74,39 @@ public class LectureControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("lecture/showLecture"))
                 .andExpect(model().attributeExists("lecture"));
+    }
+
+    @Test
+    public void testUpdateIngredientForm() throws Exception {
+        //given
+        LectureCommand lectureCommand = new LectureCommand();
+
+        //when
+        when(lectureService.findByEventIdAndLectureId(anyLong(),anyLong())).thenReturn(lectureCommand);
+
+        //then
+        mockMvc.perform(get("/event/1/lecture/2/update"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("lecture/lectureForm"))
+                .andExpect(model().attributeExists("lecture"));
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception {
+        //given
+        LectureCommand lectureCommand = new LectureCommand();
+        lectureCommand.setLectureId(3L);
+        lectureCommand.setEventId(2L);
+
+        //when
+        when(lectureService.saveLectureCommand(any())).thenReturn(lectureCommand);
+
+        //then
+        mockMvc.perform(post("/event/2/lecture")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/event/2/lecture/3/show"));
     }
 }
