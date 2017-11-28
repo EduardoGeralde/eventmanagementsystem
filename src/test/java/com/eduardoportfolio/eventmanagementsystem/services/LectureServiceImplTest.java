84b3a4ad6bbe6a2672650c73566aa28f62f4efa1
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-28 21:42:50 -0200

    testing DeleteById from Service Implementation

--- src/test/java/com/eduardoportfolio/eventmanagementsystem/services/LectureServiceImplTest.java
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/services/LectureServiceImplTest.java
@@ -108,4 +108,24 @@ public class LectureServiceImplTest {
         verify(eventDAO, times(1)).findById(anyLong());
         verify(eventDAO, times(1)).save(any(Event.class));
     }
+
+    @Test
+    public void testDeleteById() throws Exception {
+        //given
+        Event event = new Event();
+        Lecture lecture = new Lecture();
+        lecture.setLectureId(3L);
+        event.addLecture(lecture);
+        lecture.setLectureEvent(event);
+        Optional<Event> eventOptional = Optional.of(event);
+
+        when(eventDAO.findById(anyLong())).thenReturn(eventOptional);
+
+        //when
+        lectureService.deleteById(1L,3L);
+
+        //then
+        verify(eventDAO, times(1)).findById(anyLong());
+        verify(eventDAO, times(1)).save(any(Event.class));
+    }
 }
