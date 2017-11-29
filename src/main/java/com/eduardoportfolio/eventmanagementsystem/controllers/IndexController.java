7cd1eb8dae69f74ad18bf54c2e4c9e1e26b51875
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-29 18:25:50 -0200

    Annotation changed to avoid ambiguity

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/IndexController.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/IndexController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,7 +22,7 @@ public class IndexController {
         this.eventService = eventService;
     }
 
-    @RequestMapping(value = { "", "/", "index"})
+    @GetMapping({ "", "/", "index"})
     public String getIndexPage(Model model){
         log.debug("IndexController getIndexPage");
         model.addAttribute("events", eventService.getEvents());
