587cb87f9b634f39fcefd6faba5fde6bc9494d42
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-30 09:19:17 -0200

    Image Controller Test created

---
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/ImageControllerTest.java
@@ -0,0 +1,75 @@
+package com.eduardoportfolio.eventmanagementsystem.controllers;
+
+import com.eduardoportfolio.eventmanagementsystem.commands.EventCommand;
+import com.eduardoportfolio.eventmanagementsystem.services.EventService;
+import com.eduardoportfolio.eventmanagementsystem.services.ImageService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Eduardo on 30/11/17.
+ */
+public class ImageControllerTest {
+
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    EventService eventService;
+
+    ImageController imageController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageController = new ImageController(eventService,imageService);
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    public void testImageForm() throws Exception {
+        //given
+        EventCommand eventCommand = new EventCommand();
+        eventCommand.setEventId(1L);
+
+        when(eventService.findCommandById(anyLong())).thenReturn(eventCommand);
+
+        //when
+        mockMvc.perform(get("/event/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("event"));
+        verify(eventService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    public void testHandleImagePost() throws Exception {
+        MockMultipartFile multipartFile = new MockMultipartFile("imagefile","testing.txt",
+                "text/plain", "Spring Framework".getBytes());
+        mockMvc.perform(multipart("/event/1/image").file(multipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/event/1/show"));
+        verify(imageService, times(1)).saveImageFile(anyLong(),any());
+    }
+}
