80fa42ba4bc69c616c5a122f69ce93a44409fce5
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-11-30 17:36:30 -0200

    saveImageFile method implemented

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/services/ImageServiceImpl.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/services/ImageServiceImpl.java
@@ -1,8 +1,12 @@
 package com.eduardoportfolio.eventmanagementsystem.services;
 
+import com.eduardoportfolio.eventmanagementsystem.daos.EventDAO;
+import com.eduardoportfolio.eventmanagementsystem.models.Event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by Eduardo on 30/11/17.
@@ -11,8 +15,30 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService{
 
+   private final EventDAO eventDAO;
+
+    public ImageServiceImpl(EventDAO eventService) {
+        this.eventDAO = eventService;
+    }
+
     @Override
-    public void saveImageFile(Long recipeId, MultipartFile multipartFile) {
-        log.debug("Received File");
+    public void saveImageFile(Long eventId, MultipartFile multipartFile) {
+        try{
+            Event event = eventDAO.findById(eventId).get();
+
+            Byte[] byteObjects = new Byte[multipartFile.getBytes().length];
+
+            int i = 0;
+            for(byte b : multipartFile.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            event.setEventLogo(byteObjects);
+            eventDAO.save(event);
+        } catch (IOException e){
+            //todo handle better
+            log.error("Error Occurred", e);
+            e.printStackTrace();
+        }
     }
 }
