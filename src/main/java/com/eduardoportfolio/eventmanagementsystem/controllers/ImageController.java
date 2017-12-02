005c6cc3e897674c665c6da9485204f424a07d30
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-12-02 18:07:50 -0200

    renderImageFromDB action created

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/ImageController.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/ImageController.java
@@ -1,12 +1,19 @@
 package com.eduardoportfolio.eventmanagementsystem.controllers;
 
+import com.eduardoportfolio.eventmanagementsystem.commands.EventCommand;
 import com.eduardoportfolio.eventmanagementsystem.services.EventService;
 import com.eduardoportfolio.eventmanagementsystem.services.ImageService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Eduardo on 30/11/17.
@@ -36,5 +43,23 @@ public class ImageController {
         log.debug("ImageController handleImagePost");
         imageService.saveImageFile(eventId, multipartFile);
         return "redirect:/event/"+eventId+"/show";
+    }
+
+    @GetMapping("event/{eventId}/eventImage")
+    public void renderImageFromDB(@PathVariable("eventId")Long eventId, HttpServletResponse response) throws IOException{
+        log.debug("ImageController renderImageFromDB");
+        EventCommand eventCommand = eventService.findCommandById(eventId);
+
+        if (eventCommand != null) {
+            byte[] byteArray = new byte[eventCommand.getEventLogo().length];
+
+            int i = 0;
+            for (Byte wrappedByte : eventCommand.getEventLogo()) {
+                byteArray[i++] = wrappedByte;  //auto unboxing
+            }
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
