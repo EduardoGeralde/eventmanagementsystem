da2b2ce630c198248bc2ea1fd8d639b10d9ca5dc
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-12-04 06:06:26 -0200

    New Exception Handler for NumberFormatException created

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventController.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventController.java
@@ -75,4 +75,17 @@ public class EventController {
         modelAndView.addObject("exception", exception);
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
 }
