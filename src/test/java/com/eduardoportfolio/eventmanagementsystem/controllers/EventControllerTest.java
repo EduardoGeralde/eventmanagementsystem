8929ea0bfb74cbaa4a80e0171610cc630ba5bc80
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-12-04 06:07:53 -0200

    test for Number Format Exception created

--- src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventControllerTest.java
+++ src/test/java/com/eduardoportfolio/eventmanagementsystem/controllers/EventControllerTest.java
@@ -115,4 +115,12 @@ public class EventControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }
+
+    @Test
+    public void testRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/event/fsrd/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 }
