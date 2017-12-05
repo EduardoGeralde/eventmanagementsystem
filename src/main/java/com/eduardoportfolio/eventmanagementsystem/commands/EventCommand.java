48a50f677324f1cf9362079019a77b103dc0ca17
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-12-05 06:06:37 -0200

    Constraints added to the Command

--- src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/EventCommand.java
+++ src/main/java/com/eduardoportfolio/eventmanagementsystem/commands/EventCommand.java
@@ -4,9 +4,12 @@ import com.eduardoportfolio.eventmanagementsystem.models.Lecture;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -20,9 +23,16 @@ import java.util.List;
 public class EventCommand {
 
     private Long eventId;
+    @NotBlank
+    @Size(min=3, max=100)
     private String eventName;
+    @NotBlank
+    @Size(min=3, max=255)
     private String eventDescription;
+    @URL
     private String eventSite;
+    @NotBlank
+    @Size(min=3, max=100)
     private String eventLocal;
     private Byte[] eventLogo;
     @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
